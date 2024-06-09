4aa81e93d672e717038565b05c66bb9a0756f479
Leonard <L.Pingen@physik.uni-muenchen.de>
2024-06-09 17:35:11 +0200

    more data

--- sheet_7/data_generation.py
+++ sheet_7/data_generation.py
@@ -9,5 +9,8 @@ x_values = np.random.uniform(-1, 1, 100)
 fx_values = f(x_values)
 t_values = fx_values + np.random.normal(0, 1/25, 100)
 
-with open("data.dat", "w") as data_file:
-    print(*x_values, file=data_file, sep="\n")
+with open("target_data.dat", "w") as target_data_file:
+    print(*t_values, file=target_data_file, sep="\n")
+
+with open("input_data.dat", "w") as input_target_data_file:
+    print(*x_values, file=input_target_data_file, sep="\n")
