ac3fdb46b28a0a19a1f96d7e3d34c1d14e49b9aa
Leonard <L.Pingen@physik.uni-muenchen.de>
2024-06-30 15:28:29 +0200

    cnn functional

---
+++ sheet_10/cnn.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+# load and separate data
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# reshape data from (60000, 28, 28) to (60000, 28, 28, 1)
+x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
+x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+
+# normalize pixel values
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# convert labels to one-hot encoding
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+
+# visualize sample images
+fig, axes = plt.subplots(4, 4, dpi=500)
+for num, ax in enumerate(axes.flatten()):
+    ax.imshow(x_train[num].reshape(28, 28), cmap='gray')
+    ax.set(xticks=[], yticks=[])
+fig.tight_layout()
+fig.savefig('figures/sample_images.png')
+
+# build CNN
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, epochs=1, batch_size=128, validation_data=(x_test, y_test))
+
+# evaluate test accuracy
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {test_acc:.4f}')
+print(f'Test loss: {test_loss:.4f}')
